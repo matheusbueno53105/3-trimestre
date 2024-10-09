251b2e2d46309765e71cfa4d65fd104e51d926a8
matheusbueno53105 <164014421+matheusbueno53105@users.noreply.github.com>
2024-10-08 21:30:46 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,15 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
+    'O que é JavaScript',
+    'O javaScript é uma linguagem de programação'
+    )
 criaCartao(
     'Geografia',
     'Qual a capital da França?',
-    'A capital da França é Paris'
+    A capital da França é Paris'
 )
-
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
+    'o que é uma função?',
+    'Uma função é um bloco de codigo que executa alguma tarefa'
+    )
